346aaa45c44103ec93ace8dbc75d3b497c596421
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2024-06-11 09:56:20 -0500

    environments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('orders-ms');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+
+  logger.log(`Server is running on: ${envs.port}`);
 }
 bootstrap();
